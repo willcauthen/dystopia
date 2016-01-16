34c38f8a15a2b0a67c127d7970a5f73e87471328
Will Cauthen <willcauthen50@gmail.com>
2016-01-16 15:30:20 -0800

    fun with angular

--- public/js/app.js
+++ public/js/app.js
@@ -6,12 +6,17 @@ app.controller('MainCtrl', function ($scope) {
 	$scope.name = "World";
 
 	$scope.players = [
-		{name: 'Dustin', role: "muscle"},
-		{name: 'Cody', role: "magic"},
-		{name: 'Will', role: "moxie"}
+		{name: 'Dustin', role: "muscle", bool: true},
+		{name: 'Cody', role: "magic", bool: true},
+		{name: 'Will', role: "moxie", bool: true}
 	];
 	$scope.addPlayer = function () {
 		// console.log($scope.newPlayer);
 		$scope.players.push($scope.newPlayer);
+	};
+	$scope.wasClicked = function() {
+		if ($scope.player.bool === true) {
+			$scope.player.bool = false;
+		}
 	};
 });
