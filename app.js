917894e8ea13f81d09d83a3df6892f7aee34c703
will cauthen <willcauthen50@gmail.com>
2016-01-21 12:28:53 -0800

    see last

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log("We're in good shape");
+
+var app = angular.module("angularSmall", ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/");
+
+	$stateProvider
+		.state('people-index', {
+			url: "/",
+			templateUrl: "templates/people.html",
+			controller: 'PeopleIndexCtrl'
+		});
+});
+
+app.controller("MainCtrl", function ($scope) {
+	$scope.message = "WORLD!";
+
+	$scope.people = [
+	{name: 'Tony', age: 23},
+	{name: 'Marco', age: 31},
+	{name: 'Alejandro', age: 17},
+	{name: 'Marvin', age: 9}];
+
+	$scope.addPerson = function() {
+		$scope.people.push($scope.newPerson);
+		$scope.newPerson = {};
+	}
+})
